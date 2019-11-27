0d2c1710d9ed73aa2fd27ac71802ed5d8ab01c5a
Matteo Parlato <matteo.parlato@outlook.it>
2019-11-27 21:54:35 +0100

    Embed resources with Costura.Fody

--- Project Kittan/Program.cs
+++ Project Kittan/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Windows;
@@ -9,12 +8,9 @@ namespace Project_Kittan
 {
     class Program
     {
-        private static Dictionary<string, Assembly> loadedLibs = new Dictionary<string, Assembly>();
-
         [STAThread]
         public static void Main(string[] args)
         {
-            AppDomain.CurrentDomain.AssemblyResolve += Resolve;
 
             switch (args.Length)
             {
@@ -64,58 +60,6 @@ namespace Project_Kittan
                         break;
                     }
             }
-        }
-
-        private static Assembly Resolve(object sender, ResolveEventArgs e)
-        {
-            string libName = new AssemblyName(e.Name).Name;
-
-            if (loadedLibs.ContainsKey(libName)) return loadedLibs[libName];
-
-            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Project_Kittan." + libName + ".dll"))
-            {
-                try
-                {
-                    byte[] buffer = new BinaryReader(stream).ReadBytes((int)stream.Length);
-                    Assembly assembly = Assembly.Load(buffer);
-                    loadedLibs[libName] = assembly;
-                    return assembly;
-                }
-                catch (Exception) { }
-            }
-            return null;
-
-            // Blocked by Windows Defender for possible Trojan:Win32/Cloxer.D!cl
-
-            ////var thisAssembly = Assembly.GetExecutingAssembly();
-
-            ////var assemblyName = new AssemblyName(e.Name);
-            ////var dllName = assemblyName.Name + ".dll";
-
-            ////var resources = thisAssembly.GetManifestResourceNames().Where(s => s.EndsWith(dllName));
-            ////if (resources.Any())
-            ////{
-            ////    var resourceName = resources.First();
-            ////    using (var stream = thisAssembly.GetManifestResourceStream(resourceName))
-            ////    {
-            ////        if (stream == null) return null;
-            ////        var block = new byte[stream.Length];
-            ////        try
-            ////        {
-            ////            stream.Read(block, 0, block.Length);
-            ////            return Assembly.Load(block);
-            ////        }
-            ////        catch (IOException)
-            ////        {
-            ////            return null;
-            ////        }
-            ////        catch (BadImageFormatException)
-            ////        {
-            ////            return null;
-            ////        }
-            ////    }
-            ////}
-            ////return null;
         }
     }
 }
