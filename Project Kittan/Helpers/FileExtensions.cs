ff4f172c47154ba3cba0e30893aa9aa78689edbf
Matteo Parlato <matteo.parlato@outlook.it>
2021-12-10 17:58:24 +0100

    Fixed issue #32

--- Project Kittan/Helpers/FileExtensions.cs
+++ Project Kittan/Helpers/FileExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Project_Kittan.ViewModels;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -9,6 +10,11 @@ namespace Project_Kittan.Helpers
         public static List<string> GetFilesFromDirectory(string path)
         {
             return Directory.GetFiles(path, "*.txt", SearchOption.AllDirectories).Where(item => !(item.Contains(@"\.hg") || item.Contains(@"\.git"))).ToList();
+        }
+
+        public static bool WorkspaceHasReadOnlyFiles(ICollection<WorkspaceFile> workspaceFiles)
+        {
+            return workspaceFiles.Any(file => file.IsReadOnly);
         }
     }
 }
